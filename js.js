18a7c930613c46a2fe428388d0f9cfd313f545cf
Pedro <pedro3dpereira@gmail.com>
2023-02-22 18:09:47 +0000

    converting to json format

--- js.js
+++ js.js
@@ -1,1 +1,27 @@
-alert ("Hello World!");
+const toDos = [
+  {
+    id: 1,
+    text: "Buy milk",
+    isCompleted: true,
+  },
+  {
+    id: 2,
+    text: "Meeting",
+    isCompleted: true,
+  },
+  {
+    id: 3,
+    text: "Dentist",
+    isCompleted: false,
+  },
+  {
+    id: 4,
+    text: "Gaming",
+    isCompleted: true,
+  },
+];
+
+//converting to json
+
+const toDosJson = JSON.stringify(toDos);
+console.log(toDosJson);
